8c2ab8dbdc4105f2b7f5a1985644af3cc5f0414b
Görkem <gumutzkn@gmail.com>
2021-03-05 20:09:18 +0300

    home page changed

--- src/components/HomeFilter/homefilter.component.jsx
+++ src/components/HomeFilter/homefilter.component.jsx
@@ -1,8 +1,8 @@
-import React, { Component } from 'react'
-import { withRouter } from 'react-router-dom';
-import { MovieContext } from '../../context/MovieContext';
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { MovieContext } from "../../context/MovieContext";
 
-import "./homefilter.scss"
+import "./homefilter.scss";
 
 class HomeFilter extends Component {
   static contextType = MovieContext;
@@ -12,45 +12,43 @@ class HomeFilter extends Component {
 
     return (
       <section className="filter">
-        <select className="filter__select" name="years" required>
-          <option value="year" disabled defaultValue>Year</option>
+        {/* <select className="filter__select" name="years" required>
+          <option value="year" disabled defaultValue>
+            Year
+          </option>
           <option value="2015">2015</option>
           <option value="2016">2016</option>
           <option value="2017">2017</option>
         </select>
-  
+
         <select className="filter__select" name="types" required>
-          <option value="type" disabled defaultValue>Type</option>
+          <option value="type" disabled defaultValue>
+            Type
+          </option>
           <option value="horror">Horror</option>
           <option value="sci-fi">Sci-fi</option>
           <option value="biography">Biography</option>
-        </select>
-  
-        <div className="filter__searchbox">
-          <form 
-            action="/results"
-            className="search">
-            <input
-              onChange={onChange}
-              value={inputValue}
-              className="search__input" 
-              type="text" 
-              placeholder="Enter movie name here"
-              name="movieName" />
-            <button className="search__button" type="submit"><i className="fas fa-search"></i></button>
-          </form>
-        </div>
-  
-        <div className="filter__button">
-          <button         
+        </select> */}
+
+        <form action="/results" className="search">
+          <input
+            onChange={onChange}
+            value={inputValue}
+            className="search__input"
+            type="text"
+            placeholder="Enter movie name here"
+            name="movieName"
+          />
+
+          <input
+            className="search__gg"
             type="submit"
-          >
-            Search <span className="filter__button--arrow">&#8594;</span>
-          </button>
-        </div>
+            value="Search"
+          />
+        </form>
       </section>
-    )
-  }  
+    );
+  }
 }
 
 export default withRouter(HomeFilter);
